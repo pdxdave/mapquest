cc5bbb4cbfd762449f4d90837a98e2df8cdbe730
Raine Wallace <rainebeaudubs@gmail.com>
2019-07-23 13:54:39 -0400

    Progress is happening with the geolocation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./App.css";
-import MapQ from "./components/MapQ";
+// import MapQ from "./components/MapQ";
+import GeoMapQ from "./components/GeoMapQ";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      location: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://www.mapquestapi.com/geocoding/v1/address?location=40031&key=MDlg7TMtdyZT869bwROpAkQmLRa7L29B')
+      .then(response => {
+        this.setState(() => ({ location: response.data.results[0].locations[0].latLng }))
+        console.log(response.data.results[0].locations[0].latLng)
+
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <MapQ
+        {/* <MapQ
           height={500}
           width={1000}
           center={[33.8121, -117.9190]}
           baseLayer={"map"}
           zoom={12}
+        /> */}
+        <GeoMapQ
+          center={(this.state.location.lat, this.state.location.lng)}
         />
       </div>
     );
---
+++ src/components/GeoMapQ.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+
+class MapQ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...props,
+      height: 500,
+      width: 1000,
+      center: props.center,
+      baseLayer: "map",
+      zoom: 12
+    };
+  }
+
+  componentDidMount() {
+    let L = window.L;
+
+    L.mapquest.key = "MDlg7TMtdyZT869bwROpAkQmLRa7L29B";
+
+    L.mapquest.map("map", {
+      center: this.state.center,
+      layers: L.mapquest.tileLayer(this.state.baseLayer),
+      zoom: this.state.zoom
+    });
+
+    L.mapquest
+      .textMarker([33.8121, -117.9190], {
+        icon: {
+          primaryColor: "#333333",
+          secondaryColor: "#333333",
+          size: "sm"
+        },
+        draggable: false,
+        text: "DisneyLand",
+        position: "right",
+        subtext: "Let's have fun",
+        type: "marker"
+      })
+      .addTo(L.mapquest.Map.getMap());
+  }
+
+  render() {
+
+    const mapStyle = {
+      height: this.state.height,
+      width: this.state.width
+    };
+
+    return (
+      <div id="map" style={mapStyle}>
+      </div>
+    );
+  }
+}
+
+export default MapQ;
